1c7b763e536868fdc40bd71c34001dafb0ace483
Franjs88 <Franjs88@gmail.com>
2013-07-23 15:47:53 +0000

    Lectura desde index.html

--- web.js
+++ web.js
@@ -2,12 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var index;
+index = fs.readFileSync('index.html');
+var buf = new Buffer(index.length);
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
