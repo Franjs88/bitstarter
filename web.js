011fffbdbd858d1dceee99129d74cd6747955918
Franjs88 <Franjs88@gmail.com>
2013-08-02 20:09:57 +0000

    Modificado puerto escucha

--- web.js
+++ web.js
@@ -2,7 +2,6 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var fs = require('fs');
 var index;
 index = fs.readFileSync('index.html');
 
@@ -11,7 +10,7 @@ app.get('/', function(request, response) {
 });
 
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
