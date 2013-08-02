8ebbb1c166b05706b303208211c7f045b99684e7
Franjs88 <Franjs88@gmail.com>
2013-08-02 20:17:01 +0000

    Vuelta atras, no funcionaban los cambios bien

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 var index = "index.html";
+index = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
   response.send(index.toString());
